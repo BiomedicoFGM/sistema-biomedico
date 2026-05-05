811cd548405baf2bcda1d2f28caf687ec5012aee
Fredy Gonzalez <fregon82@hotmail.com>
2026-05-04 21:20:28 -0500

    Fix indent

--- app.py
+++ app.py
@@ -637,8 +637,10 @@ def reporte(codigo):
 # ------------------ RUN ------------------
 
 if __name__ == "__main__":
+
     with app.app_context():
-    db.drop_all()
-    db.create_all()
+        db.drop_all()
+        db.create_all()
+
     app.run(debug=True)
 
