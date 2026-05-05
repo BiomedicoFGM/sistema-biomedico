3598a6b7f07f710d24776d947da4c32d13cb006e
Fredy Gonzalez <fregon82@hotmail.com>
2026-05-04 21:29:58 -0500

    Force recreate db

--- app.py
+++ app.py
@@ -636,11 +636,10 @@ def reporte(codigo):
 
 # ------------------ RUN ------------------
 
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-
     app.run(debug=True)
 
