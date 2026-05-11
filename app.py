1206dba2acb10378c22d0820fecffb13bcf2d09c
Fredy Gonzalez <fregon82@hotmail.com>
2026-05-10 20:16:19 -0500

    Recrear tabla usuario con rol

--- app.py
+++ app.py
@@ -872,6 +872,11 @@ def reporte(codigo):
 # ------------------ RUN ------------------
 
 with app.app_context():
+
+    Usuario.__table__.drop(db.engine)
+
+    Usuario.__table__.create(db.engine)
+
     db.create_all()
 
 if __name__ == "__main__":
